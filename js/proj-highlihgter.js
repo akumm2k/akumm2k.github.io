f98bf8fbfa0f38ac5fef0b9d95faa2e167e6434d
akumm2k <82381989+akumm2k@users.noreply.github.com>
2021-10-23 08:35:53 +0200

    Add project highlighter

---
+++ js/proj-highlihgter.js
@@ -0,0 +1,27 @@
+const proj_list = [ 'rbTree', 'gauss', 'diceRoller', 'portfolio' ];
+const proj_nodes = [];
+proj_list.forEach(proj => {
+    proj_nodes.push(document.getElementById(proj));
+});
+
+const log = console.log;
+
+let stack = {
+    // check stack ids from index.html
+    'rbTree' : [ 'c-sharp', 'git', 'gitlab' ],
+    'gauss' : [ 'python', 'gitlab' ],
+    'diceRoller' : [ 'cpp', 'gitlab' ],
+    'portfolio' : [ 'javascript', 'html5', 'css3', 'git', 'github' ]
+}
+
+proj_nodes.forEach(proj => {
+    proj.addEventListener('mouseover', () => {
+        console.log(proj.id);
+        for (let tech of stack[proj.id]) 
+            document.getElementById(tech).classList.add('hover');
+    });
+    proj.addEventListener('mouseout', () => {
+        for (let tech of stack[proj.id]) 
+            document.getElementById(tech).classList.remove('hover');
+    });
+});
