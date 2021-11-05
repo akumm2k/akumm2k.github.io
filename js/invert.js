f33ee04f27af4b63733c87a0f3b4517965b43bb3
akumm2k <82381989+akumm2k@users.noreply.github.com>
2021-11-05 09:37:38 +0100

    Fix bugs and refactor code

--- js/invert.js
+++ js/invert.js
@@ -1,13 +1,19 @@
 let checkbox = document.querySelector("#dark-mode-checkbox");
+let body = document.body;
+const dark = "dark-mode-body", light = "light-mode-body";
 
 checkbox.addEventListener('change', () => {
-    let body = document.body;
-    body.classList.toggle("dark-mode-body");
-    body.classList.toggle("light-mode-body");
+    body.classList.toggle(dark);
+    body.classList.toggle(light);
 });
 
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     // dark mode preference
     checkbox.checked = true;
-    body.classList.toggle("dark-mode-body");
+    body.classList.toggle(dark);
+}
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
+    checkbox.checked = false;
+    body.classList.toggle(light);
 }
