8ab945124da6c8550f52b1e502c45a3873dff105
Aryan <82381989+akumm2k@users.noreply.github.com>
2022-01-29 20:49:16 +0100

    Fix timeDiff bug
    I am 19 years old.

--- js/age.js
+++ js/age.js
@@ -1,10 +1,10 @@
-const timeDiff = (d1, d2) => 
-    Math.floor(Math.abs((d1.getTime() - d2.getTime())) / (3600 * 24 * 1000));
+const yearsDiff = (d1, d2) => 
+    Math.floor(Math.abs((d1.getTime() - d2.getTime())) / (3600 * 24 * 1000 * 365));
 
 
 const birthDate = new Date("05/24/2002");
 let today = new Date();
-let years = today.getFullYear() - birthDate.getFullYear();
+let years = yearsDiff(today, birthDate);
 
 let aboutPara = document.querySelector("#about > p");
 aboutPara.textContent += ` I am ${years} years old.`
