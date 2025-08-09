def82e2a40962f4866836121c801040eae90316c
Aryan <82381989+akumm2k@users.noreply.github.com>
2025-08-09 14:13:08 +0200

    Add typescript migration draft

---
+++ ts/js/src/constants/projects.js
@@ -0,0 +1,11 @@
+export const PROJECTS = {
+    REVERSI: 'reversi',
+    MAZE_RUNNER_PAGE_READER: 'maze-runner-page-reader',
+    SUDOKU_SOLVER: 'sudoku-solver',
+    CRUD_APP: 'crud-app',
+    RB_TREE: 'rbTree',
+    GAUSS: 'gauss',
+    DICE_ROLLER: 'diceRoller',
+    PORTFOLIO: 'portfolio',
+    WEB_STUDIO: 'web-studio',
+};
---
+++ ts/js/src/proj_highlighter.js
@@ -0,0 +1,54 @@
+import { TOOLS } from './constants/tools';
+import { PROJECTS } from './constants/projects';
+const PROJ_NODES = document.querySelectorAll('ol.proj-list > li > a');
+const PROJ_STACK = {
+    [PROJECTS.REVERSI]: [TOOLS.JAVA, TOOLS.GITHUB],
+    [PROJECTS.MAZE_RUNNER_PAGE_READER]: [TOOLS.PYTHON],
+    [PROJECTS.SUDOKU_SOLVER]: [TOOLS.CPP, TOOLS.GIT, TOOLS.GITLAB],
+    [PROJECTS.CRUD_APP]: [
+        TOOLS.PHP,
+        TOOLS.JAVASCRIPT,
+        TOOLS.CSS3,
+        TOOLS.HTML5,
+        TOOLS.GIT,
+    ],
+    [PROJECTS.RB_TREE]: [TOOLS.C_SHARP, TOOLS.GIT, TOOLS.GITLAB],
+    [PROJECTS.GAUSS]: [TOOLS.PYTHON, TOOLS.GITLAB],
+    [PROJECTS.DICE_ROLLER]: [TOOLS.CPP, TOOLS.GITLAB],
+    [PROJECTS.PORTFOLIO]: [
+        TOOLS.JAVASCRIPT,
+        TOOLS.HTML5,
+        TOOLS.CSS3,
+        TOOLS.GIT,
+        TOOLS.GITHUB,
+    ],
+    [PROJECTS.WEB_STUDIO]: [
+        TOOLS.JAVASCRIPT,
+        TOOLS.HTML5,
+        TOOLS.CSS3,
+        TOOLS.GIT,
+        TOOLS.GITHUB,
+    ],
+};
+Object.keys(PROJ_STACK).forEach((project) => {
+    const projElement = document.getElementById(project);
+    console.assert(projElement !== undefined, `${project} is not present in html`);
+});
+PROJ_NODES.forEach((proj) => {
+    proj.addEventListener('mouseover', () => {
+        for (let tech of PROJ_STACK[proj.id]) {
+            const techElement = document.getElementById(tech);
+            if (techElement !== null) {
+                techElement.classList.add('hover');
+            }
+        }
+    });
+    proj.addEventListener('mouseout', () => {
+        for (let tech of PROJ_STACK[proj.id]) {
+            const techElement = document.getElementById(tech);
+            if (techElement !== null) {
+                techElement.classList.remove('hover');
+            }
+        }
+    });
+});
